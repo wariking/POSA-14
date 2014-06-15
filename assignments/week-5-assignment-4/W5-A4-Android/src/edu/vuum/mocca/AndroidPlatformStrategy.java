0a3153551463af2f5b8be4da46c18e7e27a0d040
wariking <zoubbin@gmail.com>
2014-06-15 23:55:21 +0800

    week five

--- assignments/week-5-assignment-4/W5-A4-Android/src/edu/vuum/mocca/AndroidPlatformStrategy.java
+++ assignments/week-5-assignment-4/W5-A4-Android/src/edu/vuum/mocca/AndroidPlatformStrategy.java
@@ -9,17 +9,17 @@ import android.util.Log;
 
 /**
  * @class AndroidPlatformStrategy
- * 
+ *
  * @brief Provides methods that define a platform-independent API for
  *        output data to Android UI thread and synchronizing on thread
  *        completion in the ping/pong game.  It plays the role of the
  *        "Concrete Strategy" in the Strategy pattern.
  */
 public class AndroidPlatformStrategy extends PlatformStrategy
-{	
+{
     /** TextViewVariable. */
     private TextView mTextViewOutput;
-	
+
     /** Activity variable finds gui widgets by view. */
     private WeakReference<Activity> mActivity;
 
@@ -47,24 +47,28 @@ public class AndroidPlatformStrategy extends PlatformStrategy
     {
         /** (Re)initialize the CountDownLatch. */
         // TODO - You fill in here.
-        mLatch = new CountDownLatch(2);
+        mLatch = new CountDownLatch(NUMBER_OF_THREADS);
     }
 
     /** Print the outputString to the display. */
     public void print(final String outputString)
     {
-        /** 
+        /**
          * Create a Runnable that's posted to the UI looper thread
-         * and appends the outputString to a TextView. 
+         * and appends the outputString to a TextView.
          */
         // TODO - You fill in here.
-        mTextViewOutput.append(outputString);
-        mTextViewOutput.append("\n");
+        ((Activity) mActivity.get()).runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                mTextViewOutput.append(outputString+"\n");
+            }
+        });
     }
 
     /** Indicate that a game thread has finished running. */
     public void done()
-    {	
+    {
         // TODO - You fill in here.
         mLatch.countDown();
     }
@@ -80,12 +84,12 @@ public class AndroidPlatformStrategy extends PlatformStrategy
         }
     }
 
-    /** 
+    /**
      * Error log formats the message and displays it for the
      * debugging purposes.
      */
-    public void errorLog(String javaFile, String errorMessage) 
+    public void errorLog(String javaFile, String errorMessage)
     {
-       Log.e(javaFile, errorMessage);
+        Log.e(javaFile, errorMessage);
     }
 }
